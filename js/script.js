b4739185a2a284be3140d0ba6554c307e7bc2315
greg <greg.sypek@gmail.com>
2021-08-23 21:27:40 +0200

    implementing smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Make mobile navigation works
+
 const btnNav = document.querySelector(".btn--mobile-nav");
 const header = document.querySelector(".header");
 
@@ -5,6 +7,35 @@ btnNav.addEventListener("click", function () {
 	header.classList.toggle("nav-open");
 });
 
+//Update yeerain footer
 const year = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 year.textContent = currentYear;
+
+//Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+	link.addEventListener("click", function (e) {
+		const href = link.getAttribute("href");
+
+		// Scroll back to top
+		if (href === "#") {
+			e.preventDefault();
+
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			});
+		}
+
+		// Scroll to other links
+		if (href.startsWith("#")) {
+			e.preventDefault();
+
+			const sectionEl = document.querySelector(href);
+			sectionEl.scrollIntoView({ behavior: "smooth" });
+		}
+	});
+});

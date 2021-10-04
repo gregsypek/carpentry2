b047da6eb676b2bf67a48383131a58e12c089c41
greg <greg.sypek@gmail.com>
2021-10-04 22:02:50 +0200

    implement rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const stairRouter = require('./routes/stairRoutes');
@@ -7,10 +9,17 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARES
+//GLOBAL  MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+const limiter = rateLimit({
+  max: 100, //to prevent denial of service and also brute force attacks
+  windowMs: 60 * 60 * 100,
+  message:
+    'Za duzo zapytań od tego adresu IP, Proszę spróbować później po upływie godziny',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));

7ff2b1bd5ff1754f439ffdef78ea912adffe4021
greg <greg.sypek@gmail.com>
2021-10-04 23:02:45 +0200

    parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -14,7 +15,11 @@ const app = express();
 
 //GLOBAL  MIDDLEWARES
 // Set security HTTP headers
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 //Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -36,6 +41,10 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 //Data sanitization against XSS
 app.use(xss());
+
+//Prevent parameter pollution( two the same fields in query are
+// converted into array - split(',') doesnt work in sort function )
+app.use(hpp());
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
 

21f2deb7b17461e5b533114f744a2854a3ff4ae7
greg <greg.sypek@gmail.com>
2021-10-11 20:48:18 +0200

    Logging in user part 2

--- public/js/sticky-nav.js
+++ public/js/sticky-nav.js
@@ -1,26 +1,27 @@
-// Sticky navigation
+/*eslint-disable*/
 
-const sectionHeroEl = document.querySelector(".section-hero");
+// Sticky navigation
+const sectionHeroEl = document.querySelector('.section-hero');
 
 const obs = new IntersectionObserver(
-	function (entries) {
-		const ent = entries[0];
-		console.log(ent);
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
 
-		if (!ent.isIntersecting) {
-			// document.body.classList.add("sticky");
-			document.body.classList.add("sticky");
-		}
+    if (!ent.isIntersecting) {
+      // document.body.classList.add("sticky");
+      document.body.classList.add('sticky');
+    }
 
-		if (ent.isIntersecting) {
-			document.body.classList.remove("sticky");
-		}
-	},
-	{
-		// In the viewport
-		root: null,
-		threshold: 0,
-		rootMargin: "-80px",
-	}
+    if (ent.isIntersecting) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
 );
 obs.observe(sectionHeroEl);
